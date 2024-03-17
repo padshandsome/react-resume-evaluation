1084110feb1cb61ee1527d6d09c9eb8a8cf73315
Mengmeng Liu <723291747@qq.com>
2024-03-17 16:10:39 -0400

    resolve warnings

--- src/components/Evaluation.jsx
+++ src/components/Evaluation.jsx
@@ -41,13 +41,13 @@ function Evaluation() {
                     </div>
                     <ul tabindex="0" class="z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                         <li>
-                        <a href="" class="justify-between">
+                        <a href="/evaluate" class="justify-between">
                             Profile
                             <span class="badge">New</span>
                         </a>
                         </li>
-                        <li><a href="">Settings</a></li>
-                        <li><a href="">Logout</a></li>
+                        <li><a href="/evaluate">Settings</a></li>
+                        <li><a href="/evaluate">Logout</a></li>
                     </ul>
                     </div>
                 </div>
